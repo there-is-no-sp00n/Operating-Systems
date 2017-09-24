dd5b486db6111aa4b64021c11391f31f840aa79b
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-09-23 22:01:13 -0500

    Update sys.c

--- sys.c
+++ sys.c
@@ -10,8 +10,28 @@ SYSCALL_DEFINE2(procstat, int, PID, struct proc_stat*, proccc)
 {
 	printk( KERN_WARNING "IN DEFINE2\n");
 	struct proc_stat kernel;	
+	
 	kernel.state = find_task_by_pid_ns(PID, &init_pid_ns)->state;
-	long t = kernel.state;
-	printk( KERN_WARNING "STATE = %ld \n", t);
+	long a = kernel.state;
+	printk( KERN_WARNING "STATE = %ld \n", a);
+	
+	kernel.priority = find_task_by_pid_ns(PID, &init_pid_ns)->prio;
+	long b = kernel.priority;
+	printk( KERN_WARNING "PRIORITY = %ld \n", b);
+
+	kernel.normal_priority = find_task_by_pid_ns(PID, &init_pid_ns)->normal_prio;
+	long c = kernel.normal_priority;
+	printk(KERN_WARNING "NORMAL PRIORITY = %ld \n", c);
+
+	kernel.static_priority = find_task_by_pid_ns(PID, &init_pid_ns)->static_prio;
+	long d = kernel.static_priority;
+	printk(KERN_WARNING "STATIC PRIORITY = %ld \n", d);
+
+	kernel.rt_priority = find_task_by_pid_ns(PID, &init_pid_ns)->rt_priority;
+	long e = kernel.rt_priority;
+	printk(KERN_WARNING "RT PRIORITY = %ld \n", e);
+
+	//kernel.policy = find_task_by_pid_ns(PID, &init_pid_ns)->policy
+	
 	return 0;	
 }
