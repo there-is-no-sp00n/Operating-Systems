c3360bd03aa3d0a400833d4a88c3bd0bad752a38
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-09-24 23:15:06 -0500

    Update sys.c

--- sys.c
+++ sys.c
@@ -8,6 +8,13 @@ SYSCALL_DEFINE0(hello)
 
 SYSCALL_DEFINE2(procstat, int, PID, struct proc_stat*, proccc)
 {
+	if(find_task_by_pid_ns(PID, &init_pid_ns) == NULL)//find_task_by_pid_ns(PID, &init_pid_ns)->parent->pid == NULL && find_task_by_pid_ns(PID, &init_pid_ns)->pid != 1)
+	{
+		printk(KERN_WARNING "PID IS NULL\n");
+		int errno = ESRCH;
+		return errno;
+	}
+	
 	printk( KERN_WARNING "IN DEFINE2\n");
 	struct proc_stat kernel;	
 	
@@ -39,5 +46,44 @@ SYSCALL_DEFINE2(procstat, int, PID, struct proc_stat*, proccc)
 	kernel.time_slice = temp.time_slice;
 	int g = kernel.time_slice;
 	printk(KERN_WARNING "TIME SLICE = %d \n", g);
-	return 0;	
+
+	kernel.pid = find_task_by_pid_ns(PID, &init_pid_ns)->pid;
+	int h = kernel.pid;
+	printk(KERN_WARNING "PID = %d \n", h);
+
+	kernel.num_context_switches = (find_task_by_pid_ns(PID, &init_pid_ns)->nvcsw) + (find_task_by_pid_ns(PID, &init_pid_ns)->nivcsw);
+	int i = kernel.num_context_switches;
+	printk(KERN_WARNING "CONTEXT SWITCHES = %d\n", i);
+
+	kernel.user_time = find_task_by_pid_ns(PID, &init_pid_ns)->utime;
+	long j = kernel.user_time;
+	printk(KERN_WARNING "USER TIME = %ld\n",j);
+
+	kernel.sys_time = find_task_by_pid_ns(PID, &init_pid_ns)->stime;
+	long k = kernel.sys_time;
+	printk(KERN_WARNING "SYSTEM TIME = %ld\n", k);
+
+	//kernel.total_pages_mapped = find_task_by_pid_ns(PID, &init_pid_ns)->numa_pages_migrated;
+	//long l = kernel.total_pages_mapped;
+	//printk(KERN_WARNING "TOTAL PAGES MAPPED = %ld\n", l);
+	
+	strcpy(kernel.name, find_task_by_pid_ns(PID, &init_pid_ns)->comm);
+	printk(KERN_WARNING "NAME = %s", &kernel.name);
+
+	kernel.parent_pid = find_task_by_pid_ns(PID, &init_pid_ns)->real_parent->pid;
+	int m = kernel.parent_pid;
+	printk(KERN_WARNING "PARENT PID = %d", m);
+	
+	/*proccc->state = a;//kernel.state;
+	proccc->priority = b;//kernel.priority;
+	proccc->normal_priority = c;//kernel.priority;
+	proccc->static_priority = d;//kernel.static_priority;
+	proccc->rt_priority = e;//kernel.rt_priority;
+	proccc->time_slice = f;//kernel.time_slice;
+	proccc->policy = g;//kernel.policy; */
+	
+
+	copy_to_user(proccc, &kernel, sizeof(struct proc_stat));
+	
+	return 0;
 }
